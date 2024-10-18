8103cee49f9b627a99e9d937c78d29011d9f26a1
SunilBaghel002 <sunilbaghel93100@gmail.com>
2024-10-18 22:56:11 +0530

    new project added

---
+++ Re-start/dom and bom/script.js
@@ -0,0 +1,7 @@
+const button=document.querySelector(".btn")
+
+button.style.padding="10px"
+button.style.backgroundColor="red"
+button.style.color="white"
+button.style.borderRadius="10px"
+button.innerText=""
