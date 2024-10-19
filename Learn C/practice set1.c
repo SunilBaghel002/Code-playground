302e1f5e8230db8f3cf023538c7b98d80c27aefe
SunilBaghel002 <sunilbaghel93100@gmail.com>
2024-10-19 21:21:45 +0530

    there are some practice sets

---
+++ Learn C/practice set1.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+
+int main(){
+    // int a=3;
+    // int b=4;
+    // printf("area of rectangle1 is %d\n", a*b);
+
+    // int l;
+    // int br;
+    // printf("enter the length of rectangle: ");
+    // scanf("%d",&l);
+    // printf("enter the breadth of rectangle: ");
+    // scanf("%d",&br);
+    // printf("Area of rectangle is %d\n",l*br);
+
+
+    // int r;
+    // float area;
+    // printf("Enter the value of Radius : ");
+    // scanf("%d", &r);
+    // area= 22*r*r/7;
+    // printf("Area of circle is %f\n", area);
+
+    // float volume;
+    // float h;
+    // printf("Enter the height of cylinder: ");
+    // scanf("%f", &h);
+    // volume= 22*r*r*h/7;
+    // printf("Volume of cylinder is %f\n", volume);
+
+    // float c;
+    // float f;
+    // printf("Enter the value of °F(Farenheit): ");
+    // scanf("%f", &f);
+    // c= (f-32) *5/9;
+    // printf("Then, value of °C(Celcius) is %f", c);
+
+    // int principle, years;
+    // float rateOfInterest, simpleInterest;
+
+    // printf("Principle= ");
+    // scanf("%d", &principle);
+    // printf("No. of years= ");
+    // scanf("%d", &years);
+    // printf("Rate of Interest= ");
+    // scanf("%f", &rateOfInterest);
+    // simpleInterest= principle*years*rateOfInterest/100;
+    // printf("Simple Interest= %f", simpleInterest);
+    return 0;     
+}
--- Re-start/dom and bom/script.js
+++ Re-start/dom and bom/script.js
@@ -1,7 +1,25 @@
-const button=document.querySelector(".btn")
+// let container=document.querySelector(".container")
+// const para=document.createElement("p")
+// container.appendChild(para)
+// para.innerHTML="this is paragraph"
+// const para2=document.createElement("article")
+// container.append(para2)
+// para2.innerHTML="this is a article"
 
-button.style.padding="10px"
-button.style.backgroundColor="red"
-button.style.color="white"
-button.style.borderRadius="10px"
-button.innerText=""
+// const para3=document.createElement("anchor")
+// container.prepend(para3)
+// para3.textContent="djias"
+
+// const para4=document.createElement("input")
+// container.before(para4)
+// para4.style.backgroundColor="white"
+// para4.placeholder="this is a placeholder"
+
+// const para5=document.createElement("button")
+// container.after(para5)
+// para5.textContent="this is a button"
+// para.remove()
+// para2.remove()
+// para3.remove()
+// para4.remove()
+// para5.remove()
